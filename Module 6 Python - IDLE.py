233c8cbcaa2671faea9a47bd7305ab928197750a
TreyL14 <tlohrey@ivytech.edu>
2025-04-28 13:53:39 -0400

    Add files via upload

---
+++ Module 6 Python - IDLE.py
@@ -0,0 +1,11 @@
+#set variable c to "cats"
+c = "cats"
+
+#set variable d to "dogs"
+d = "dogs"
+
+#set variable s to full sentence
+s = "It is raining" + c + "and" + d + "."
+
+#print full sentence
+print(s)
